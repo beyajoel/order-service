6293389c3945b2ab18f8bebf3c6b9de6956edf92
joelbeya <joelbeya.bj@gmail.com>
2022-10-29 20:37:29 +0200

    first push

---
+++ src/test/java/com/bent/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bent.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        OrderServiceApplication.main(new String[]{"args"});
+    }
+
+}
