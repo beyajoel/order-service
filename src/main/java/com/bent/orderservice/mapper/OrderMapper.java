8189400ed2a1b175aba613d16d8da900b62a0e07
joelbeya <joelbeya.bj@gmail.com>
2022-10-31 15:58:45 +0100

    fix mapper

--- src/main/java/com/bent/orderservice/mapper/OrderMapper.java
+++ src/main/java/com/bent/orderservice/mapper/OrderMapper.java
@@ -7,6 +7,4 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
     OrderLineItems fromRequest(OrderLineItemsDto orderLineItemsDto);
-
-    OrderLineItemsDto toRequest(OrderLineItems orderLineItems);
 }
